d5da2224a2f0701d6307d4a143f16db8f71fe75d
Rutuja Umap <80196441+Rutuja949@users.noreply.github.com>
2024-03-19 14:59:29 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-0cd59ecaf368e5ccf"
   instance_type          = "t2.medium"
   key_name               = "key73"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
